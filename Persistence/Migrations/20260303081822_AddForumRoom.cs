2d9a88c326061af4b57ec15d6b82b36261d73aec
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-03-03 11:51:35 +0330

    feat(persistence): configure ForumRoom and add migration.

---
+++ Persistence/Migrations/20260303081822_AddForumRoom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForumRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ForumRooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Subtitle = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Rules = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(2048)", maxLength: 2048, nullable: true),
+                    IsClosed = table.Column<bool>(type: "bit", nullable: false),
+                    IsPinned = table.Column<bool>(type: "bit", nullable: false),
+                    IsDisabled = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumRooms", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ForumRooms");
+        }
+    }
+}
--- Persistence/Providers/DataContext.cs
+++ Persistence/Providers/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.User;
+﻿using Domain.Forum;
+using Domain.User;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Providers;
@@ -14,11 +15,11 @@ public class DataContext : DbContext
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Role> Roles { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<ForumRoom> ForumRooms { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         #region AppUser
-
         builder.Entity<AppUser>(user =>
         {
             user.HasKey(x => x.Id);
@@ -44,11 +45,9 @@ public class DataContext : DbContext
             user.Property(x => x.PasswordHashed)
                 .IsRequired();
         });
-
         #endregion
 
         #region Role
-
         builder.Entity<Role>(role =>
         {
             role.HasKey(r => r.Id);
@@ -56,11 +55,9 @@ public class DataContext : DbContext
             role.Property(r => r.Name)
                 .IsRequired();
         });
-
         #endregion
 
         #region UserRole join table
-
         builder.Entity<UserRole>(entity =>
         {
             entity.HasKey(x => new { x.UserId, x.RoleId });
@@ -73,7 +70,29 @@ public class DataContext : DbContext
                 .WithMany(x => x.UserRoles)
                 .HasForeignKey(x => x.RoleId);
         });
-        
+        #endregion
+
+        #region ForumRoom
+        builder.Entity<ForumRoom>(entity =>
+        {
+            entity.HasKey(f => f.Id);
+
+            entity.Property(f => f.Title)
+                .HasMaxLength(256)
+                .IsRequired();
+
+            entity.Property(f => f.Subtitle)
+                .HasMaxLength(256);
+
+            entity.Property(f => f.Rules)
+                .HasMaxLength(2048);
+
+            entity.Property(f => f.Description)
+                .HasMaxLength(2048);
+
+            entity.Property(f => f.IsDisabled)
+                .HasDefaultValue(false);
+        });
         #endregion
     }
 }
