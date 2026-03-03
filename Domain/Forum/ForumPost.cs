8f41b8e70b474141e0debae9d92b19f337a709c9
pouria7gh <pouria.ghafarzadeh@gmail.com>
2026-03-03 11:26:29 +0330

    chore(domain): remove data annotation.

--- Domain/Forum/ForumPost.cs
+++ Domain/Forum/ForumPost.cs
@@ -7,26 +7,12 @@ namespace Domain.Forum
 {
     public class ForumPost
     {
-        [Required]
-        [Display(Name = "محتوای پست")]
-        [StringLength(maximumLength: 8192, MinimumLength = 2, ErrorMessage = "{0} باید حداقل {2} و حداکثر {1} حرف باشد.")]
         public string PostContent { get; set; } = string.Empty;
 
         #region Foreign Keys
-
-        [Required]
-        [Display(Name = "اتاق گفتگو")]
-        public Guid FroumRoomId { get; set; }
-
-        [ForeignKey(nameof(FroumRoomId))]
-        [Display(Name = "اتاق گفتگو")]
+        public Guid ForumRoomId { get; set; }
         public virtual ForumRoom ForumRoom { get; set; } = default!;
-
-        [Display(Name = "کاربر")]
         public Guid? UserId { get; set; }
-
-        [ForeignKey(nameof(UserId))]
-        [Display(Name = "کاربر")]
         public virtual AppUser? User { get; set; }
 
         public Guid? ParentPostId { get; set; }
@@ -35,19 +21,9 @@ namespace Domain.Forum
         #endregion
 
         public virtual List<ForumPost>? Replies { get; set; }
-
-        [Display(Name = "توضیحات")]
-        [MaxLength(2048)]
         public string? Description { get; set; }
-
-        [Display(Name = "غیر فعال")]
-        [DefaultValue(false)]
         public bool IsDisabled { get; set; } = false;
-
-        [Display(Name = "تاریخ ایجاد")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        [Display(Name = "تاریخ ویرایش")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- Domain/Forum/ForumPostInteraction.cs
+++ Domain/Forum/ForumPostInteraction.cs
@@ -7,32 +7,14 @@ namespace Domain.Forum
 {
     public class ForumPostInteraction : BaseEntity
     {
-        [Display(Name = "کاربر")]
         public Guid? UserId { get; set; }
-
-        [ForeignKey(nameof(UserId))]
-        [Display(Name = "کاربر")]
         public virtual AppUser? User { get; set; }
-
-        public Guid ForumPostId { get; set; }
-        [ForeignKey(nameof(ForumPostId))]
         public virtual ForumPost ForumPost { get; set; } = default!;
-
         public bool IsLiked { get; set; }
         public bool IsDisliked { get; set; }
-
-        [Display(Name = "توضیحات")]
-        [MaxLength(2048)]
         public string? Description { get; set; }
-
-        [Display(Name = "غیر فعال")]
-        [DefaultValue(false)]
         public bool IsDisabled { get; set; } = false;
-
-        [Display(Name = "تاریخ ایجاد")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        [Display(Name = "تاریخ ویرایش")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
 }
